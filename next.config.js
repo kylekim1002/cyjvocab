5e7348f1bf032d782cfb9cd6b6c94a0bef4861f1
Kyle <cyjkyle@gmail.com>
2026-01-16 12:46:36 +0900

    Next.js webpack 경로 별칭 설정 추가
    - @/ 경로 해석을 webpack에 명시적으로 설정
    - UI 컴포넌트 경로 인식 문제 해결

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
       bodySizeLimit: '10mb',
     },
   },
+  // TypeScript 경로 별칭 해석 명시
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname),
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
